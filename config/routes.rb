3b15eb59348d1cdcdae0403bde34a8a78e9f14c9
Victor Prete <vprete@tuta.io>
2022-08-09 16:21:52 -0400

    Add article controller crud methods and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  # get 'articles/list'
+  # get 'articles/show'
+  # get 'articles/create'
+  # get 'articles/update'
+  # get 'articles/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :articles
 end
